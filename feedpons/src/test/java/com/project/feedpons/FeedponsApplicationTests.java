d79aba7b0a170a00f7547958a3ca6017ed171e64
foodease <60240443+foodease@users.noreply.github.com>
2020-03-09 15:58:35 -0400

    Add files via upload

---
+++ feedpons/src/test/java/com/project/feedpons/FeedponsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.feedpons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedponsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
